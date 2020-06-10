eced1fa0ffed43dc53b8143c5dde3f06a5bc3a3c
tomoki maekawa <maekawatomoki@team-lab.com>
2020-06-10 20:56:46 +0900

    Initial Commit

---
+++ metal-demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  metal-demo
+//
+//  Created by 前川　知紀 on 2020/06/10.
+//  Copyright © 2020 前川　知紀. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ metal-demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  metal-demo
+//
+//  Created by 前川　知紀 on 2020/06/10.
+//  Copyright © 2020 前川　知紀. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
