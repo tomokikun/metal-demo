0d21dba1f5a622aebc55c2a3b31607a23c298939
tomoki maekawa <maekawatomoki@team-lab.com>
2020-09-01 10:30:11 +0900

    textureを準備

--- FilteringWithMetal/FilteringWithMetal/Shader.metal
+++ FilteringWithMetal/FilteringWithMetal/Shader.metal
@@ -23,75 +23,28 @@ vertex VertexIn vertexShader(device float4 *position [[ buffer(0) ]],
     };
 }
 
-half4 normalFilter3x3(float2 pos, texture2d<float> tex) {
+fragment half4 fragmentShader(VertexIn vertexIn [[ stage_in ]],
+                              texture2d<float, access::sample> texture [[ texture(0) ]]) {
     constexpr sampler defaultSampler;
-    
-    const float width = 6016.0;
-    const float height = 3384.0;
-    const float dw = 1.0 / width;
-    const float dh = 1.0 / height;
-    
-    const float3x3 normalF = float3x3(float3(1.0/9.0, 1.0/9.0, 1.0/9.0),
-                                      float3(1.0/9.0, 1.0/9.0, 1.0/9.0),
-                                      float3(1.0/9.0, 1.0/9.0, 1.0/9.0)) * 0.35;
-    // 0 1 2
-    // 3 4 5
-    // 6 7 8
-    float4 c_0 = float4(tex.sample(defaultSampler, pos + float2(-dw, -dh)));
-    float4 c_1 = float4(tex.sample(defaultSampler, pos + float2(0.0, -dh)));
-    float4 c_2 = float4(tex.sample(defaultSampler, pos + float2(dw, -dh)));
-    float4 c_3 = float4(tex.sample(defaultSampler, pos + float2(-dw, 0.0)));
-    float4 c_4 = float4(tex.sample(defaultSampler, pos + float2(0.0, 0.0)));
-    float4 c_5 = float4(tex.sample(defaultSampler, pos + float2(dw, 0.0)));
-    float4 c_6 = float4(tex.sample(defaultSampler, pos + float2(-dw, dh)));
-    float4 c_7 = float4(tex.sample(defaultSampler, pos + float2(0.0, dh)));
-    float4 c_8 = float4(tex.sample(defaultSampler, pos + float2(dw, dh)));
-
-    float3x3 r = float3x3(float3(c_0.r, c_1.r, c_2.r),
-                          float3(c_3.r, c_4.r, c_5.r),
-                          float3(c_6.r, c_7.r, c_8.r)) * normalF;
-    float3x3 g = float3x3(float3(c_0.g, c_1.g, c_2.g),
-                          float3(c_3.g, c_4.g, c_5.g),
-                          float3(c_6.g, c_7.g, c_8.g)) * normalF;
-    float3x3 b = float3x3(float3(c_0.b, c_1.b, c_2.b),
-                          float3(c_3.b, c_4.b, c_5.b),
-                          float3(c_6.b, c_7.b, c_8.b)) * normalF;
-    
-    float sr = 0;
-    float sg = 0;
-    float sb = 0;
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            sr += r[i][j];
-            sg += g[i][j];
-            sb += b[i][j];
-        }
-    }
-
-    return half4(sr, sg, sb, 1.0);;
+    vertexIn.texCoor.y = 1 - vertexIn.texCoor.y;
+    half4 color = half4(texture.sample(defaultSampler, vertexIn.texCoor.xy));
+    return color;
 }
 
-//half4 normalFilter5x5(float2 pos, texture2d<float> tex) {
+//inline half4 normalFilter3x3(float2 pos, texture2d<float> tex) {
 //    constexpr sampler defaultSampler;
 //
-////    const float width = 6016.0;
-////    const float height = 3384.0;
-//    const float width = 256.0;
-//    const float height = 256.0;
+//    const float width = 6016.0;
+//    const float height = 3384.0;
 //    const float dw = 1.0 / width;
 //    const float dh = 1.0 / height;
 //
-//    const float3x3 normalF = float3x3(float3(1.0/25.0, 1.0/25.0, 1.0/25.0),
-//                                      float3(1.0/25.0, 1.0/25.0, 1.0/25.0),
-//                                      float3(1.0/25.0, 1.0/25.0, 1.0/25.0));
-//    // 00 01 02 03 04
-//    // 05 06 07 08 09
-//    // 10 11 12 13 14
-//    // 15 16 17 18 19
-//    // 20 21 22 23 24
-//
-//    // -1, 1, 0, -2, 2
+//    const float3x3 normalF = float3x3(float3(1.0/9.0, 1.0/9.0, 1.0/9.0),
+//                                      float3(1.0/9.0, 1.0/9.0, 1.0/9.0),
+//                                      float3(1.0/9.0, 1.0/9.0, 1.0/9.0)) * 0.35;
+//    // 0 1 2
+//    // 3 4 5
+//    // 6 7 8
 //    float4 c_0 = float4(tex.sample(defaultSampler, pos + float2(-dw, -dh)));
 //    float4 c_1 = float4(tex.sample(defaultSampler, pos + float2(0.0, -dh)));
 //    float4 c_2 = float4(tex.sample(defaultSampler, pos + float2(dw, -dh)));
@@ -101,36 +54,16 @@ half4 normalFilter3x3(float2 pos, texture2d<float> tex) {
 //    float4 c_6 = float4(tex.sample(defaultSampler, pos + float2(-dw, dh)));
 //    float4 c_7 = float4(tex.sample(defaultSampler, pos + float2(0.0, dh)));
 //    float4 c_8 = float4(tex.sample(defaultSampler, pos + float2(dw, dh)));
-//    float4 c_9 = float4(tex.sample(defaultSampler, pos + float2(-dw, -dh)));
-//    float4 c_10 = float4(tex.sample(defaultSampler, pos + float2(0.0, -dh)));
-//    float4 c_11 = float4(tex.sample(defaultSampler, pos + float2(dw, -dh)));
-//    float4 c_12 = float4(tex.sample(defaultSampler, pos + float2(-dw, 0.0)));
-//    float4 c_13 = float4(tex.sample(defaultSampler, pos + float2(0.0, 0.0)));
-//    float4 c_14 = float4(tex.sample(defaultSampler, pos + float2(dw, 0.0)));
-//    float4 c_15 = float4(tex.sample(defaultSampler, pos + float2(-dw, dh)));
-//    float4 c_16 = float4(tex.sample(defaultSampler, pos + float2(0.0, dh)));
-//    float4 c_17 = float4(tex.sample(defaultSampler, pos + float2(dw, dh)));
-//    float4 c_18 = float4(tex.sample(defaultSampler, pos + float2(-dw, -dh)));
-//    float4 c_19 = float4(tex.sample(defaultSampler, pos + float2(0.0, -dh)));
-//    float4 c_20 = float4(tex.sample(defaultSampler, pos + float2(dw, -dh)));
-//    float4 c_21 = float4(tex.sample(defaultSampler, pos + float2(-dw, 0.0)));
-//    float4 c_22 = float4(tex.sample(defaultSampler, pos + float2(0.0, 0.0)));
-//    float4 c_23 = float4(tex.sample(defaultSampler, pos + float2(dw, 0.0)));
-//    float4 c_24 = float4(tex.sample(defaultSampler, pos + float2(-dw, dh)));
 //
-//    float r = float(c_0.r, c_1.r, c_2.r, c_3.r, c_4.r, c_5.r,
-//                    c_6.r, c_7.r, c_8.r, c_9.r, c_10.r, c_11.r,
-//                    c_12.r, c_13.r, c_14.r, c_15.r, c_16.r, c_17.r,
-//                    c_18.r, c_19.r, c_20.r, c_21.r, c_22.r, c_23.r)
-////    float3x3 r = float3x3(float3(c_0.r, c_1.r, c_2.r),
-////                          float3(c_3.r, c_4.r, c_5.r),
-////                          float3(c_6.r, c_7.r, c_8.r)) * normalF;
-////    float3x3 g = float3x3(float3(c_0.g, c_1.g, c_2.g),
-////                          float3(c_3.g, c_4.g, c_5.g),
-////                          float3(c_6.g, c_7.g, c_8.g)) * normalF;
-////    float3x3 b = float3x3(float3(c_0.b, c_1.b, c_2.b),
-////                          float3(c_3.b, c_4.b, c_5.b),
-////                          float3(c_6.b, c_7.b, c_8.b)) * normalF;
+//    float3x3 r = float3x3(float3(c_0.r, c_1.r, c_2.r),
+//                          float3(c_3.r, c_4.r, c_5.r),
+//                          float3(c_6.r, c_7.r, c_8.r)) * normalF;
+//    float3x3 g = float3x3(float3(c_0.g, c_1.g, c_2.g),
+//                          float3(c_3.g, c_4.g, c_5.g),
+//                          float3(c_6.g, c_7.g, c_8.g)) * normalF;
+//    float3x3 b = float3x3(float3(c_0.b, c_1.b, c_2.b),
+//                          float3(c_3.b, c_4.b, c_5.b),
+//                          float3(c_6.b, c_7.b, c_8.b)) * normalF;
 //
 //    float sr = 0;
 //    float sg = 0;
@@ -147,11 +80,26 @@ half4 normalFilter3x3(float2 pos, texture2d<float> tex) {
 //    return half4(sr, sg, sb, 1.0);;
 //}
 
-fragment half4 fragmentShader(VertexIn vertexIn [[ stage_in ]],
-                              texture2d<float, access::sample> texture [[ texture(0) ]]) {
-    constexpr sampler defaultSampler;
-    vertexIn.texCoor.y = 1 - vertexIn.texCoor.y;
-    half4 color = half4(texture.sample(defaultSampler, vertexIn.texCoor.xy));
-    color = normalFilter3x3(vertexIn.texCoor.xy, texture);
-    return color;
+kernel void blur5x5(texture2d<float, access::read> inTexture [[ texture(0) ]],
+                    texture2d<float, access::write> outTexture [[ texture(1) ]],
+                    texture2d<float, access::read> weights [[ texture(2) ]],
+                    uint2 gid [[ thread_position_in_grid ]] ) {
+    int size = weights.get_width();
+    int radius = size / 2;
+    
+    float4 accumColor(0, 0, 0, 0);
+    
+    for (int j = 0; j < size; j++)
+    {
+       for (int i = 0; i < size; i++)
+       {
+           uint2 kernelIndex(i, j);
+           uint2 textureIndex(gid.x + (i - radius), gid.y + (j - radius));
+           float4 color = inTexture.read(textureIndex).rgba;
+           float4 weight = weights.read(kernelIndex).rrrr;
+           accumColor += weight * color;
+       }
+    }
+
+    outTexture.write(float4(accumColor.rgb, 1), gid);
 }
--- FilteringWithMetal/FilteringWithMetal/ViewController.swift
+++ FilteringWithMetal/FilteringWithMetal/ViewController.swift
@@ -28,25 +28,30 @@ private let indexData: [UInt16] = [
     2, 3, 1,
 ]
 
+private let weights: [Float] = [
+    1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0,
+    1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0,
+    1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0,
+    1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0,
+    1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0, 1.0 / 25.0,
+]
 
 class ViewController: NSViewController, MTKViewDelegate {
     
     private let width: Int = 800
     private let height: Int = 500
     private let name: String = "High Sierra"
-    
     private let device: MTLDevice = MTLCreateSystemDefaultDevice()!
     private var commandQueue: MTLCommandQueue!
     private var mtkView: MTKView!
     private var vertexBuffer: MTLBuffer!
     private var textureCoorBuffer: MTLBuffer!
     private var indexBuffer: MTLBuffer!
-    private var texPosBuffer: MTLBuffer!
     private var pipelineState: MTLRenderPipelineState!
-    private var texture: MTLTexture!
-    
-    let blurRadius = 10
-    
+    private var computePipelineState: MTLComputePipelineState!
+    private var inTexture: MTLTexture!
+    private var outTexture: MTLTexture!
+    private var weightsBuffer: MTLBuffer!
     
     override func loadView() {
         view = MTKView(frame: NSRect(x: 0, y: 0, width: width, height: height), device: device)
@@ -57,11 +62,11 @@ class ViewController: NSViewController, MTKViewDelegate {
         setupMetal()
         makeBuffers()
         makePipeline()
+        makeComputePipelineState()
         loadTexture(name: name)
     }
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        
     }
     
     func draw(in view: MTKView) {
@@ -81,7 +86,7 @@ class ViewController: NSViewController, MTKViewDelegate {
         vertexBuffer = device.makeBuffer(bytes: vertexData, length: MemoryLayout<Float>.size * vertexData.count)
         textureCoorBuffer = device.makeBuffer(bytes: textureCoorData, length: MemoryLayout<Float>.size * textureCoorData.count)
         indexBuffer = device.makeBuffer(bytes: indexData, length: MemoryLayout<UInt16>.size * indexData.count)
-        texPosBuffer = device.makeBuffer(bytes: textureCoorData, length: MemoryLayout<Float>.size * textureCoorData.count)
+        weightsBuffer = device.makeBuffer(bytes: weights, length: MemoryLayout<Float>.size * weights.count)
     }
     
     private func makePipeline() {
@@ -94,38 +99,107 @@ class ViewController: NSViewController, MTKViewDelegate {
         pipelineState = try! device.makeRenderPipelineState(descriptor: descriptor)
     }
     
+    private func makeComputePipelineState() {
+        guard let library = device.makeDefaultLibrary() else { return }
+        guard let function = library.makeFunction(name: "blur5x5") else { return }
+        do {
+            computePipelineState = try device.makeComputePipelineState(function: function)
+        } catch let error {
+            print(error)
+            fatalError()
+        }
+    }
+    
     private func render(to view: MTKView) {
         guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
         
-        guard let drawable = view.currentDrawable else { return }
+        // compute
         do {
-            guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
-            renderPassDescriptor.colorAttachments[0].texture = drawable.texture
-            renderPassDescriptor.colorAttachments[0].loadAction = .clear
-            renderPassDescriptor.colorAttachments[0].storeAction = .store
-            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 1.0, 1.0)
-
-            let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
-            renderCommandEncoder.setRenderPipelineState(pipelineState)
-            renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-            renderCommandEncoder.setVertexBuffer(texPosBuffer, offset: 0, index: 1)
-
-            renderCommandEncoder.setFragmentTexture(texture, index: 0)
-            print(texture.width)
-            print(texture.height)
-            renderCommandEncoder.drawIndexedPrimitives(type: .triangleStrip, indexCount: indexData.count, indexType: .uint16, indexBuffer: indexBuffer, indexBufferOffset: 0)
-            renderCommandEncoder.endEncoding()
+            let encoder = commandBuffer.makeComputeCommandEncoder()
+            encoder?.setComputePipelineState(computePipelineState)
+            encoder?.setBuffer(weightsBuffer, offset: 0, index: 0)
+            
+            let threadgroupSize = MTLSize(width: 256, height: 256, depth: 1)
+            let threadgroupCount = MTLSize(width: (inTexture.width + threadgroupSize.width - 1) / threadgroupSize.width, height: (inTexture.height + threadgroupSize.height - 1) / threadgroupSize.height, depth: 1)
+            
+            
+            let descriptor: MTLTextureDescriptor = .texture2DDescriptor(pixelFormat: inTexture.pixelFormat, width: inTexture.width, height: inTexture.height, mipmapped: false)
+            descriptor.usage = [.shaderRead, .shaderWrite]
+            descriptor.storageMode = .managed
+            outTexture = device.makeTexture(descriptor: descriptor)!
+            encoder!.setTexture(inTexture, index: 0)
+            encoder!.setTexture(outTexture, index: 1)
+            encoder!.dispatchThreadgroups(threadgroupSize, threadsPerThreadgroup: threadgroupCount)
+            encoder!.endEncoding()
         }
-        commandBuffer.present(drawable)
+                
+//        guard let drawable = view.currentDrawable else { return }
+//        do {
+//            guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+//            renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+//            renderPassDescriptor.colorAttachments[0].loadAction = .clear
+//            renderPassDescriptor.colorAttachments[0].storeAction = .store
+//            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 1.0, 1.0)
+//
+//            let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+//            renderCommandEncoder.setRenderPipelineState(pipelineState)
+//            renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+//            renderCommandEncoder.setVertexBuffer(textureCoorBuffer, offset: 0, index: 1)
+//
+//            renderCommandEncoder.setFragmentTexture(outTexture, index: 0)
+//            renderCommandEncoder.drawIndexedPrimitives(type: .triangleStrip, indexCount: indexData.count, indexType: .uint16, indexBuffer: indexBuffer, indexBufferOffset: 0)
+//            renderCommandEncoder.endEncoding()
+//        }
+        commandBuffer.addCompletedHandler(saveImage)
+//        commandBuffer.present(drawable)
         commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+        
     }
     
     private func loadTexture(name: String) {
         let textureLoader = MTKTextureLoader(device: device)
         let scaleFactor: CGFloat = 2
-        texture = try! textureLoader.newTexture(name: name, scaleFactor: scaleFactor, bundle: nil)
+        inTexture = try! textureLoader.newTexture(name: name, scaleFactor: scaleFactor, bundle: nil)
         //        mtkView.colorPixelFormat = texture.pixelFormat
     }
     
+    
+    private func saveImage(_commandBuffer: MTLCommandBuffer) {
+        
+        guard let tex: MTLTexture = outTexture else { return }
+        DispatchQueue.main.async{
+            let commandBuffer: MTLCommandBuffer = _commandBuffer.commandQueue.makeCommandBuffer()!
+            let blitEncoder = commandBuffer.makeBlitCommandEncoder()!
+            blitEncoder.synchronize(resource: tex)
+            blitEncoder.endEncoding()
+            commandBuffer.addCompletedHandler({_ in
+                self.saveImage(tex)
+            })
+            commandBuffer.commit()
+        }
+    }
+    
+    private func saveImage(_ tex: MTLTexture) {
+        autoreleasepool(invoking: {
+            var count = 1
+            guard let outImage = CIImage(mtlTexture: tex, options: nil),
+                    let jpgData = CIContext().jpegRepresentation(of: outImage, colorSpace: outImage.colorSpace ?? CGColorSpaceCreateDeviceRGB(), options: [:]) else { return }
+            
+            do {
+                let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+                let filename = NSString(format: "/output/%05d.jpg", count) as String
+                defer {
+                    count += 1
+                }
+                let url: URL = URL(fileURLWithPath: path+filename)
+                try jpgData.write(to: url)
+            } catch let err {
+                print(err)
+                print("fail to save image.")
+            }
+        })
+    }
 }
+
 
